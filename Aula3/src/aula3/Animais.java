c2d778cb5363feb6d24badbb0b159795a71fef32
Gabriel-Fiaux <gfiauxreis@gmail.com>
2024-04-04 21:25:05 -0300

    Primeiro Commit!
    Adicionei a aplicação Java Aula 3.

---
+++ Aula3/src/aula3/Animais.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula3;
+
+/**
+ *
+ * @author aluno
+ */
+public class Animais {
+
+    public void comer() {
+        System.out.println("O animal está Comendo");
+    }
+
+    public void dormir() {
+        System.out.println("O animal está Dormindo");
+    }
+
+    public void acordar() {
+        System.out.println("O animal está Acordado");
+    }
+
+    public void ruido() {
+        System.out.println("O animal está fazendo ruido ");
+    }
+    
+    public void mover(){
+        System.out.println("O animal está se Movendo");
+    }
+}
---
+++ Aula3/src/aula3/Hipopotamo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula3;
+
+/**
+ *
+ * @author aluno
+ */
+public class Hipopotamo extends Animais {
+
+    public Hipopotamo(){
+        super();
+        System.out.println("É um hipopotamo");
+    }
+    
+    @Override
+    public void ruido() {
+        System.out.println("Ronc Ronc Ronc ");
+    }
+    
+    
+}
+
